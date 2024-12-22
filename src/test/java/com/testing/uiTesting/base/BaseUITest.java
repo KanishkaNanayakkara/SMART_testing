e877e3f6ec73ad881c89721440f94b38c9de8d55
KanishkaNanayakkara <kanishkananayakkara98@gmail.com>
2024-12-22 23:11:28 +0530

    refactor ui login test

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -1,29 +1,22 @@
 package com.testing.uiTesting.base;
 
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseUITest {
 
+    //why this is protected?
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        // Setup Chrome WebDriver
-        WebDriverManager.chromedriver().setup();
-        
-        // Configure Chrome options
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-extensions");
-        
-        // Initialize driver
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
