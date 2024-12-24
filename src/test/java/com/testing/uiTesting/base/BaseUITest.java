82616bb4031909020c678bdedd74882f449fdadc
KanishkaNanayakkara <kanishkananayakkara98@gmail.com>
2024-12-24 18:21:25 +0530

    revert headless ui fix

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.Cookie;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseUITest {
@@ -23,6 +22,11 @@ public class BaseUITest {
         // Set up WebDriverManager to handle ChromeDriver
         WebDriverManager.chromedriver().setup();
 
+         // Configure ChromeOptions
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+ 
+         driver = new ChromeDriver(options);
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
