1f2639f8c3a877daf48106aae471d9816b7ea98f
ChaviruAtthanayake <chaviruatthanayake@gmail.com>
2024-12-24 23:41:04 +0530

    create and change the files

--- src/test/java/com/testing/apiTesting/tests/NonExistenceBookUpdateTests.java
+++
@@ -1,7 +0,0 @@
-package com.testing.apiTesting.tests;
-
-import com.testing.apiTesting.base.BaseAPITest;
-
-public class NonExistenceBookUpdateTests extends BaseAPITest {
-    
-}
--- src/test/java/com/testing/apiTesting/tests/put/GeneralPutAPITests.java
+++ src/test/java/com/testing/apiTesting/tests/put/GeneralPutAPITests.java
@@ -6,11 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.testing.apiTesting.apiClients.BookAPIClient;
 import com.testing.apiTesting.base.BaseAPITest;
 import com.testing.apiTesting.utils.APIResponseValidator;
+import com.testing.apiTesting.utils.BookDataFactory;
 import com.testing.apiTesting.utils.TestUtils;
 
 public class GeneralPutAPITests extends BaseAPITest {
@@ -59,5 +61,50 @@ public class GeneralPutAPITests extends BaseAPITest {
         Response getUpdatedResponse = bookAPIClient.getBookById(username, createdBookId);
         APIResponseValidator.updateBookTest(getUpdatedResponse);
     }
+
+    @Test(description = "Verify updating a non-existent book")
+    public void testUpdateNonexistentBook() {
+
+        Map<String, Object> invalidBookData = new HashMap<>();
+        invalidBookData.put("title", "Invalid Update Title");
+        invalidBookData.put("author", "Invalid Update Author");
+
+        int nonExistentBookId = 99999;
+
+        Response updateResponse = bookAPIClient.updateBook(username, nonExistentBookId, invalidBookData);
+        APIResponseValidator.notExistBookUpdate(updateResponse);
+       
+    }
+
+    @DataProvider(name = "invalidBookData")
+    public Object[][] invalidBookDataProvider() {
+        return new Object[][]{
+                {1, BookDataFactory.createBookWithMissingTitle(username), "Title is required"},
+                {2, BookDataFactory.createBookWithMissingAuthor(username), "Author is required"},
+                {3, BookDataFactory.createBookWithMissingTitleAndAuthor(), "Title and Author are required"}
+        };
+    }
+    
+    @Test(dataProvider = "invalidBookData", description = "Verify updating a book with invalid data")
+    public void testInputValidation(int bookId, Map<String, Object> bookData, String expectedErrorMessage) {
+        Response response = bookAPIClient.updateBook(username, bookId, bookData);
+        APIResponseValidator.bookValidation(response);
+    }
+
+    @Test(description = "Verify updating a book with unauthorized access")
+    public void testUpdateBookUnauthorizedAccess() {
+        
+        int bookId = 1;
+        Map<String, Object> bookData = new HashMap<>();
+        bookData.put("title", "Unauthorized Update");
+        bookData.put("author", "Unknown");
+
+        Response response = bookAPIClient.updateBook("", bookId, bookData);
+        APIResponseValidator.unauthorizedUpdate(response);
+       
+    }
+
+    
+
     
 }
