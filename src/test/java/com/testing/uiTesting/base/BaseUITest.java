067643a9b69e9aece7377efe3ef348bf47267747
KanishkaNanayakkara <kanishkananayakkara98@gmail.com>
2024-12-24 16:36:01 +0530

    fied headless issue

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -23,11 +23,7 @@ public class BaseUITest {
         // Set up WebDriverManager to handle ChromeDriver
         WebDriverManager.chromedriver().setup();
 
-        // Configure ChromeOptions
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
-
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
