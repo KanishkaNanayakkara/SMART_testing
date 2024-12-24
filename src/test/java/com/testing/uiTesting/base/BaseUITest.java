2be6a8046f9cbf52721df9855ece0855846ac017
WenuraSubhasinghe <wenu.subasinghe@gmail.com>
2024-12-24 07:08:54 +0530

    fix ui test not running issue

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -7,14 +7,21 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseUITest {
 
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
+        // Set up WebDriverManager to handle ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Configure ChromeOptions
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
