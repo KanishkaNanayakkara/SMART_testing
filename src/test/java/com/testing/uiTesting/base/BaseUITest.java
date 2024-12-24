36536a41e4d614e9877daa98724c11978a299798
KanishkaNanayakkara <kanishkananayakkara98@gmail.com>
2024-12-24 18:29:26 +0530

    change ci file

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.Cookie;
@@ -19,14 +18,7 @@ public class BaseUITest {
 
     @BeforeMethod
     public void setUp() {
-        // Set up WebDriverManager to handle ChromeDriver
-        WebDriverManager.chromedriver().setup();
-
-         // Configure ChromeOptions
-         ChromeOptions options = new ChromeOptions();
-         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
- 
-         driver = new ChromeDriver(options);
+        
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
