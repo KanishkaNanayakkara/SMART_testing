b342b750b9df9a9615153e0d457ee050ec7351b8
Thaaru Fernando <fernandoatm.20@uom.lk>
2024-12-24 06:59:30 +0530

    put test cases into common classes

---
+++ src/test/java/com/testing/apiTesting/tests/put/GeneralPutAPITests.java
@@ -0,0 +1,63 @@
+package com.testing.apiTesting.tests.put;
+
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.testing.apiTesting.apiClients.BookAPIClient;
+import com.testing.apiTesting.base.BaseAPITest;
+import com.testing.apiTesting.utils.APIResponseValidator;
+import com.testing.apiTesting.utils.TestUtils;
+
+public class GeneralPutAPITests extends BaseAPITest {
+    private BookAPIClient bookAPIClient;
+    private TestUtils testUtils;
+
+    String username = "admin";
+
+    @BeforeMethod
+    public void setUp(){
+        bookAPIClient = new BookAPIClient(this);
+        testUtils = new TestUtils();
+    }
+
+    @Test(description = "Verify updating a book with invalid data")
+    public void testInvalidBookUpdate() {
+
+        Response createValiedBook = testUtils.createTestBook(bookAPIClient, "Test Book 8", "Test Author 8", username);
+
+        Integer createdBookId = createValiedBook.jsonPath().getInt("id");
+        
+        Map<String, Object> invalidBookData = new HashMap<>();
+        invalidBookData.put("id", createdBookId);
+        invalidBookData.put("title", ""); 
+        invalidBookData.put("author", null);
+
+        Response updateResponse = bookAPIClient.updateBook(username, createdBookId, invalidBookData);
+        APIResponseValidator.invalidBookUpdate(updateResponse);
+        
+    }
+
+    @Test(description = "Verify updating a book with valid data")
+    public void testValidBookUpdate() {
+
+        Response createValiedBook = testUtils.createTestBook(bookAPIClient, "Test Book 9", "Test Author 9", username);
+        
+        Integer createdBookId = createValiedBook.jsonPath().getInt("id");
+
+        Map<String, Object> updatedBookData = new HashMap<>();
+        updatedBookData.put("id", createdBookId);
+        updatedBookData.put("title", "Updated Book Title");
+        updatedBookData.put("author", "Updated Author");
+
+        bookAPIClient.updateBook(username, createdBookId, updatedBookData);
+
+        Response getUpdatedResponse = bookAPIClient.getBookById(username, createdBookId);
+        APIResponseValidator.updateBookTest(getUpdatedResponse);
+    }
+    
+}
