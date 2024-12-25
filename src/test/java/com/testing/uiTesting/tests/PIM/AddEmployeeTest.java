934c9d8451913741fb4f33fced7fd002bf1b20f1
KanishkaNanayakkara <kanishkananayakkara98@gmail.com>
2024-12-25 17:28:58 +0530

    added new test

---
+++ src/test/java/com/testing/uiTesting/tests/PIM/AddEmployeeTest.java
@@ -0,0 +1,62 @@
+package com.testing.uiTesting.tests.PIM;
+
+import java.io.IOException;
+import java.text.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.testing.uiTesting.base.BaseUITest;
+import com.testing.uiTesting.pages.PIM.AddEmployeePage;
+import com.testing.uiTesting.utils.Utils;
+
+public class AddEmployeeTest extends BaseUITest{
+     
+    @Test(priority = 1)
+    public void addEmployee() throws Exception {
+        AddEmployeePage addEmployeePage = new AddEmployeePage(driver);
+        addEmployeePage.navigateToAddEmployeePage();
+
+        String firstName = "Radha";
+        String lastName = "Kamani";
+        String employeeId= "UOM" +Utils.generateRandomNumber(100,4999);
+        addEmployeePage.enterEmployeeDetails(firstName, lastName, employeeId);
+
+        String imagePath = System.getProperty("user.dir") + "/src/test/resources/Images/employeePhoto.png";
+        System.out.println("Image file path: " + imagePath);
+
+        addEmployeePage.uploadEmployeeImage(imagePath);
+        addEmployeePage.clickSaveButton();
+
+        String profileHeading = addEmployeePage.getProfileHeading();
+        // String confirmationMessage = addEmployeePage.getConfirmationMessage();
+
+        if (profileHeading.contains("Personal Details")) {
+            System.out.println("Employee added successfully!");
+        } else {
+            System.out.println("Failed to add employee!");
+        }
+
+        Assert.assertEquals(profileHeading, "Personal Details", "Added employee profile is not vissible!");
+        // Assert.assertTrue(confirmationMessage.contains("Successfully Saved"),
+        // "Addition successful!");
+    }
+
+    @Test(priority = 2)
+    public void createEmployeeWithoutFirtsName() throws InterruptedException, IOException, ParseException {
+        AddEmployeePage addEmployeePage = new AddEmployeePage(driver);
+        addEmployeePage.navigateToAddEmployeePage();
+        String lastName = "Nanayakkara";
+        String employeeId= "UOM" +Utils.generateRandomNumber(100,999);
+        addEmployeePage.enterLastName(lastName);
+        addEmployeePage.enterEmployeeId( employeeId);
+
+        String imagePath = System.getProperty("user.dir") + "/src/test/resources/Images/employeePhoto.png";
+        System.out.println("Image file path: " + imagePath);
+
+        addEmployeePage.uploadEmployeeImage(imagePath);
+        addEmployeePage.clickSaveButton();
+
+        String displayedErrorMessage = addEmployeePage.getRequiredValidationErrorMessage();
+        Assert.assertEquals(displayedErrorMessage, "Required", "Validation is not happening!");
+    }
+
+}
