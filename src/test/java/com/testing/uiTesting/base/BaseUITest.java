426ccf0939ba6ed4805dad66764bac931545729e
WenuraSubhasinghe <wenu.subasinghe@gmail.com>
2025-01-04 01:42:26 +0530

    persist login credentials for every test case

--- src/test/java/com/testing/uiTesting/base/BaseUITest.java
+++ src/test/java/com/testing/uiTesting/base/BaseUITest.java
@@ -6,19 +6,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.Cookie;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseUITest {
 
-    protected WebDriver driver;
+    protected static WebDriver driver;
     private static Set<Cookie> savedCookies;
+    private static boolean isFirstTest = true;
 
-    @BeforeClass
-    public void setUp() {
-
+    @BeforeSuite
+    public void suiteSetUp() {
         System.setProperty("allure.results.directory", "target/allure-results");
         WebDriverManager.chromedriver().setup();
 
@@ -33,34 +34,41 @@ public class BaseUITest {
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+    }
 
-        if (savedCookies != null) {
+    @BeforeMethod
+    public void beforeMethod() {
+        if (isFirstTest) {
+            driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            performLogin();
+            savedCookies = driver.manage().getCookies();
+            isFirstTest = false;
+        } else {
+            driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
             for (Cookie cookie : savedCookies) {
                 driver.manage().addCookie(cookie);
             }
             driver.navigate().refresh();
-    
+            
             if (!driver.getCurrentUrl().contains("dashboard")) {
                 performLogin();
                 savedCookies = driver.manage().getCookies();
             }
-        } else {
-            performLogin();
-            savedCookies = driver.manage().getCookies();
         }
-    
-        driver.manage().window().maximize();
     }
 
-    @AfterClass
-    public void tearDown() {
+    @AfterSuite
+    public void suiteTearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
 
     private void performLogin() {
+        if (!driver.getCurrentUrl().contains("login")) {
+            driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        }
         driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
         driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
